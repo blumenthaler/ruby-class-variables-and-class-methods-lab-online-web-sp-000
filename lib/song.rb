f5534282b5cf37d9df0a02091d3205bce4c2e38d
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-02 18:31:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
   def self.genre_count
     genre_count = Hash.new(0)
     @@genres.each do |genre|
-      
+      genre
     end
     
   # iterate over @@genres & add key/value pairs to genre_count hash
