f6d76c5f4d46fb1b1523c314a0726a96a7a8e6b5
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-02 18:21:40 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @@count += 1
+    @@genres << @genre
+  end
+  
+  def count
+    @@count
+  end
+  
+  def genres
+    
+  end
+  
+end
